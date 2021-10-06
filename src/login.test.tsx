46e75c2cedfc05a6683901a2a5ef643896e061a7
Manish Rawal <manish.rawal@convergetree.com>
2021-10-06 21:11:47 +0545

    'login-component-tests-using-queryselector-for-elements-inside-component'

---
+++ src/login.test.tsx
@@ -0,0 +1,43 @@
+import {Login} from './login'
+import * as ReactDOM from 'react-dom'
+import React from 'react'
+
+
+describe('Login component tests', () => {
+
+    let container: HTMLDivElement
+   
+
+    beforeEach(() => {
+        container = document.createElement('div');
+        document.body.appendChild(container);
+        ReactDOM.render(<Login />, container);
+    })
+
+    afterEach(() => {
+       document.body.removeChild(container);
+       container.remove()
+    })
+
+    it('Renders correctly initial document ',() => {
+        const inputs = container.querySelectorAll('input');
+        expect(inputs).toHaveLength(3)
+        expect(inputs[0].name).toBe('login')
+        expect(inputs[1].name).toBe('password')
+        expect(inputs[2].value).toBe('Login')
+
+        const label = container.querySelector('label');
+        expect(label).not.toBeInTheDocument()
+    })
+
+    it('Renders correctly initial document with data-test query',() => {
+       expect(container.querySelector("[data-test='login-form']")).toBeInTheDocument();
+       expect(container.querySelector("[data-test='login-input']")?.getAttribute('name'))
+       .toBe('login');
+       expect(container.querySelector("[data-test='password-input']")?.getAttribute('name'))
+       .toBe('password');
+    })
+
+
+
+})
